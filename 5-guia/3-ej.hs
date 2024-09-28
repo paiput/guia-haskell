e51d5c943ed271262462163302901fb9d6b5f48c
Lucas <lucaspiputto@gmail.com>
2024-09-28 17:10:16 -0300

    Agrega problemas del ejercicio 3 guia-5

--- 5-guia/3-ej.hs
+++ 5-guia/3-ej.hs
@@ -8,3 +8,21 @@
 sumatoria :: [Integer] -> Integer
 sumatoria [] = 0
 sumatoria (x:xs) = x + sumatoria xs
+
+-- 4. sumarN :: Integer -> [Integer] -> [Integer] seg´un la siguiente especificaci´on:
+-- problema sumarN (n: Z, s: seq⟨Z⟩) : seq⟨Z⟩ {
+-- requiere: { T rue }
+-- asegura: {|resultado| = |s| ∧ cada posici´on de resultado contiene el valor que hay en esa posici´on en s sumado
+-- n }
+-- }
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN _ [] = []
+sumarN n (x:xs) = (n + x):(sumarN n xs)
+
+-- multiplosDeN :: Integer -> [Integer] -> [Integer] que dado un n´umero n y una lista xs, devuelve una lista
+-- con los elementos de xs m´ultiplos de n.
+multiplosDeN :: Integer -> [Integer] -> [Integer]
+multiplosDeN _ [] = []
+multiplosDeN n (x:xs)
+  | mod x n == 0 = x:(multiplosDeN n xs)
+  | otherwise = multiplosDeN n xs
